aedb0440e5edd972fa8661a4270f99beb532d78c
Stephen Pearce <spearce@me.com>
2018-07-14 12:57:21 +1000

    processing trades

--- ui.R
+++ ui.R
@@ -104,8 +104,8 @@ dashboardPage(title = "REBBL Fantasy League",
               dashboardSidebar(
                 sidebarMenu(
                   id = "tabs",
-                  # menuItem("Leaderboard", tabName = "leaderboard", icon = icon("trophy", class = "fa-fw fa-lg")),
-                  # menuItem("Team Performance", tabName = "teams", icon = icon("football-ball", class = "fa-fw fa-lg")),
+                  menuItem("Leaderboard", tabName = "leaderboard", icon = icon("trophy", class = "fa-fw fa-lg")),
+                  menuItem("Team Performance", tabName = "teams", icon = icon("football-ball", class = "fa-fw fa-lg")),
                   menuItem("Player Stats", tabName = "stats", icon = icon("chart-bar", class = "fa-fw fa-lg", type = "regular")),
                   menuItemOutput("team_builder_menu"),
                   menuItemOutput("team_management_menu")
@@ -124,8 +124,8 @@ dashboardPage(title = "REBBL Fantasy League",
                 useShinyjs(),
                 tabItems(
                   overall_stats,
-                  #leaderboard,
-                  #team_summary,
+                  leaderboard,
+                  team_summary,
                   team_builder,
                   trade_helper
                 )
