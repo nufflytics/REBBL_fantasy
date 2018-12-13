b7d7bf463bcc4fad29f7cefe349a0d4bd112b464
sparce <pea25i@crab-bm.nexus.csiro.au>
2018-12-13 11:22:56 +1100

    Remove team creation

--- ui.R
+++ ui.R
@@ -107,7 +107,7 @@ dashboardPage(title = "REBBL Fantasy League",
                   menuItem("Leaderboard", tabName = "leaderboard", icon = icon("trophy", class = "fa-fw fa-lg")),
                   menuItem("Team Performance", tabName = "teams", icon = icon("football-ball", class = "fa-fw fa-lg")),
                   menuItem("Player Scouting", tabName = "stats", icon = icon("binoculars", class = "fa-fw fa-lg", type = "regular")),
-                  menuItemOutput("team_builder_menu"),
+                  #menuItemOutput("team_builder_menu"),
                   menuItemOutput("team_management_menu"),
                   NULL
                 )
@@ -127,7 +127,7 @@ dashboardPage(title = "REBBL Fantasy League",
                   overall_stats,
                   leaderboard,
                   team_summary,
-                  team_builder,
+                  #team_builder,
                   trade_helper
                 )
               )
